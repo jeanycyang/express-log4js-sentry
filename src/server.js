45dc85b5a42242295e55de3399fc3c8cd01fa7ec
Jean Yang <bdlas321@gmail.com>
2018-12-24 17:33:45 +0800

    feat(sentry): integrate sentry for express

--- src/server.js
+++ src/server.js
@@ -1,40 +1,66 @@
 const express = require('express');
 const log4js = require('log4js');
+const Sentry = require('@sentry/node');
 const initLogSettings = require('./initLogSettings');
 
 const PORT = process.env.PORT || 3003;
 
 initLogSettings();
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 const log = log4js.getLogger('server');
 const app = express();
+
+app.use(Sentry.Handlers.requestHandler());
 
 app.use((req, res, next) => {
   log.info(`${req.protocol} ${req.ip} ${req.path}`);
   next();
 });
 
-app.get('/error', (req, res, next) => {
+app.get('/user-error', (req, res, next) => {
   try {
     const malformedJSON = '{"data":"oops",}';
-    JSON.parse(malformedJSON);
-  } catch (error) {
-    log.error(error);
-    res.sendStatus(400);
+    const json = JSON.parse(malformedJSON);
+    res.json(json);
+  } catch (err) {
+    const error = new Error(err.message);
+    error.name = 400;
+    next(error);
   }
-  next();
 });
 
-app.get('/fatal', (req, res, next) => {
-  log.fatal(new Error('fatal!'));
-  res.sendStatus(500);
-  next();
+app.get('/server-error', (req, res, next) => {
+  const error = new Error('some server errors');
+  error.name = 500;
+  next(error);
 });
 
-app.get('/', (req, res, next) => {
+app.get('/uncaught', () => {
+  throw new Error('Uncaught error');
+});
+
+app.get('/', (req, res) => {
   log.info('some information');
   res.sendStatus(200);
-  next();
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.path} not found`);
+  error.name = 404;
+  next(error);
+});
+
+app.use(Sentry.Handlers.errorHandler());
+
+// customized error handler
+app.use((err, req, res, next) => { // eslint-disable-line
+  if (err.name >= 500) {
+    log.error(err);
+  }
+  res.status(err.name).json({
+    message: err.message,
+  });
 });
 
 app.listen(PORT, () => {
